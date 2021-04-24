43597c2b1a96ea963e439c3a2e566ab32f5beeaf
mas666-ma <642492850@qq.com>
2021-04-24 22:56:47 +0800

    new

--- COM-Debug-Assistant/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ COM-Debug-Assistant/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace COM_Debug_Assistant {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace COM_Debug_Assistant {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.1.0")]
         public static void Main() {
             COM_Debug_Assistant.App app = new COM_Debug_Assistant.App();
             app.InitializeComponent();
